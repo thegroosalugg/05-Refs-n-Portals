4a99d7b00d61f8721574f00915530d299dbac902
victor loginov <tkoe88@zoho.com>
2024-01-04 19:22:36 +0100

    updated set player name component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [inputValue, setValue] = useState(''); // initial input value must be an an empty string, and not completely empty
+  const [playerName, setName] = useState('Stranger'); // initial player name set
+
+  function handleChange(event) {
+    setValue(event.target.value); // on change the value of the player name is stored in the change event value
+  }
+
+  function handleClick() {    // conditional check set to revert name back to default is input field is an empty string
+    setName(!inputValue ? 'Stranger' : inputValue); // when button is clicked, the stored input value state is then set as the name
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={inputValue} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
