6d8650ebd26e087d6fe0b2b9d50dd749ba335f3b
victor loginov <tkoe88@zoho.com>
2024-01-07 01:03:10 +0100

    added CSS and formatter to log

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,9 +1,11 @@
+const formatter = (number) => number.toString().padStart(2, '0'); // adds leading zero to keep all styles in line with double digits
+
 export default function Log({ scores }) {
   return (
-    <ul id="">
+    <ul id="log">
       {scores.map((score, index) => (
         <li key={index}>
-          Timer: {score.timer} seconds / Score: {score.score}
+          Timer: {formatter(score.timer)} seconds -- Score: {formatter(score.score)}
         </li>
       ))}
     </ul>
