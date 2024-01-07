975030fa2c6ca475752e6b3612c15443aded7205
victor loginov <tkoe88@zoho.com>
2024-01-07 01:19:15 +0100

    further updates to Log style. Color for readability

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,13 +1,16 @@
-const formatter = (number) => number.toString().padStart(2, '0'); // adds leading zero to keep all styles in line with double digits
+const formatter = (number) => number.toString().padStart(2, "0"); // adds leading zero to keep all styles in line with double digits
 
 export default function Log({ scores }) {
-  const reversedScores = [...scores].reverse().slice(0, 10);; // Create a reversed copy of the scores array. Sliced at 10 scores max
+  const reversedScores = [...scores].reverse().slice(0, 10); // Create a reversed copy of the scores array. Sliced at 10 scores max
 
   return (
     <ul id="log">
       {reversedScores.map((score, index) => (
         <li key={index}>
-          Timer: {formatter(score.timer)} seconds -- Score: {formatter(score.score)}
+          Timer:{" "}
+          <span style={{ color: "#46cebe" }}>{formatter(score.timer)}</span>{" "}
+          seconds :: Score:{" "}
+          <span style={{ color: "#46cebe" }}>{formatter(score.score)}</span>
         </li>
       ))}
     </ul>
