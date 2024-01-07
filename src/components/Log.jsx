31eed772b46cf8dca72c2b3042a18849a80c304e
victor loginov <tkoe88@zoho.com>
2024-01-07 01:10:24 +0100

    most recent score displays at top. Log limited to 10 scores on display

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,9 +1,11 @@
 const formatter = (number) => number.toString().padStart(2, '0'); // adds leading zero to keep all styles in line with double digits
 
 export default function Log({ scores }) {
+  const reversedScores = [...scores].reverse().slice(0, 10);; // Create a reversed copy of the scores array. Sliced at 10 scores max
+
   return (
     <ul id="log">
-      {scores.map((score, index) => (
+      {reversedScores.map((score, index) => (
         <li key={index}>
           Timer: {formatter(score.timer)} seconds -- Score: {formatter(score.score)}
         </li>
